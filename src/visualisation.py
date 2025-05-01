e4137faed7a2ac929375e0678f9ea02c9d44a5c3
Abhinit_Mahajan <82913786+AbhinitMahajan@users.noreply.github.com>
2025-04-30 20:52:56 -0400

    Update visualisation.py

--- src/visualisation.py
+++ src/visualisation.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
+import os, sys 
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
 
 def plot_training_history(history):
     """
