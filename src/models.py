1f2467af701956182660124f44658e8c2c091f1a
Abhinit_Mahajan <82913786+AbhinitMahajan@users.noreply.github.com>
2025-05-01 10:20:25 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -6,16 +6,24 @@ wrapper class (AutoencoderModel) for easier model management.
 """
 
 import tensorflow as tf
-from tensorflow.keras import layers, models, constraints, regularizers, backend as K
+from tensorflow.keras import layers, models, constraints, regularizers
 from tensorflow.keras.models import load_model
 from tensorflow.keras.losses import MeanSquaredError
-from tensorflow.keras.layers import Lambda
+import math
 from src import config
 
 
 # ============================================================================
 # Custom Loss Layers
 # ============================================================================
+
+class IdentityLayer(layers.Layer):
+    """
+    Pass-through layer to name tensors.
+    """
+    def call(self, inputs):
+        return tf.identity(inputs)
+
 
 class ConsistencyLossLayer(layers.Layer):
     """
@@ -28,7 +36,7 @@ class ConsistencyLossLayer(layers.Layer):
 
     def call(self, inputs):
         deep_out, linear_out = inputs
-        # 1 - cosine similarity
+        # 1 - cosine similarity per sample
         loss = 1 - tf.reduce_sum(
             tf.nn.l2_normalize(deep_out, axis=-1) * tf.nn.l2_normalize(linear_out, axis=-1),
             axis=-1
@@ -50,7 +58,7 @@ class CorrelationLossLayer(layers.Layer):
         model_input, deep_out = inputs
         # squeeze to (batch, features)
         x_true = tf.squeeze(model_input, axis=-1)
-        x_pred = tf.squeeze(deep_out,   axis=-1)
+        x_pred = tf.squeeze(deep_out, axis=-1)
 
         # center
         mean_true = tf.reduce_mean(x_true, axis=0, keepdims=True)
@@ -79,12 +87,19 @@ class CorrelationLossLayer(layers.Layer):
 # Model Building Functions
 # ============================================================================
 
-def build_autoencoder(n_clusters, input_dim, lambda1=0.5, lambda2=0.6):
+def build_autoencoder(
+    n_clusters,
+    input_dim,
+    lambda1=0.5,
+    lambda2=0.6,
+    linear_l1=1e-5,
+    linear_l2=1e-3
+):
     inp = layers.Input(shape=(input_dim, 1), name='AE_Input')
     x = inp
     skips = []
 
-    # encoder
+    # Encoder: conv + residual + pool
     for filters in [32, 64, 128, 256]:
         y = layers.Conv1D(filters, 3, padding='same')(x)
         y = layers.LeakyReLU(negative_slope=0.2)(y)
@@ -96,7 +111,7 @@ def build_autoencoder(n_clusters, input_dim, lambda1=0.5, lambda2=0.6):
         skips.append(y)
         x = layers.MaxPooling1D(2)(y)
 
-    # bottleneck
+    # Bottleneck
     y = layers.Conv1D(512, 3, padding='same')(x)
     y = layers.LeakyReLU(negative_slope=0.2)(y)
     y = layers.Add()([
@@ -108,39 +123,50 @@ def build_autoencoder(n_clusters, input_dim, lambda1=0.5, lambda2=0.6):
     dense = layers.LeakyReLU(negative_slope=0.2)(layers.Dense(256)(flat))
     latent = layers.Dense(n_clusters, name='latent')(dense)
 
-    # dual decoder
-    z = layers.LeakyReLU(negative_slope=0.2)(layers.Dense(2*512)(latent))
+    # Dual decoder: deep reconstruction
+    z = layers.LeakyReLU(negative_slope=0.2)(layers.Dense(2 * 512)(latent))
     z = layers.Reshape((2, 512))(z)
-    for i, filters in enumerate([512, 256, 128, 64]):
+    decoder_filters = [512, 256, 128, 64]
+    for i, filters in enumerate(decoder_filters):
         z = layers.UpSampling1D(2)(z)
-        skip = skips[-(i+1)]
-        s = layers.UpSampling1D(2)(skip)
-        # dynamic crop
+        skip_tensor = skips[-(i + 1)]
+        s = layers.UpSampling1D(2)(skip_tensor)
+
+        # dynamic crop skip to match z
         def crop_fn(tensors):
             s_t, z_t = tensors
             diff = tf.shape(s_t)[1] - tf.shape(z_t)[1]
             start = diff // 2
             return s_t[:, start:start + tf.shape(z_t)[1], :]
+
         s = layers.Lambda(crop_fn, name=f'crop_skip_{i}')([s, z])
         z = layers.Concatenate()([z, s])
         z = layers.LeakyReLU(negative_slope=0.2)(layers.Conv1D(filters, 3, padding='same')(z))
 
-    z = layers.UpSampling1D(2)(z)
-    z = layers.Lambda(lambda t: t[:, :input_dim, :], name='final_crop')(z)
-    deep_out = layers.Flatten()(layers.Conv1D(1, 3, padding='same')(z))
+    # Dynamic extra upsampling to next power of two >= input_dim
+    length_loop = 2 ** (len(decoder_filters) + 1)
+    overshoot = 1 << (input_dim - 1).bit_length()
+    extra_ups = int(math.log2(overshoot / length_loop))
+    for _ in range(extra_ups):
+        z = layers.UpSampling1D(2)(z)
 
-    # linear branch
+    # Final crop back to original length
+    z = layers.Lambda(lambda t: t[:, :input_dim, :], name='final_crop')(z)
+    deep_conv = layers.Conv1D(1, 3, padding='same', name='conv_deep')(z)
+    deep_out = layers.Flatten(name='deep_flat')(deep_conv)
+
+    # Linear decoder branch
     lin_out = layers.Dense(
         input_dim,
         activation='linear',
         kernel_constraint=constraints.NonNeg(),
-        kernel_regularizer=regularizers.l1_l2(1e-5, 1e-3),
+        kernel_regularizer=regularizers.l1_l2(l1=linear_l1, l2=linear_l2),
         name='linear_output'
     )(latent)
 
-    # loss wrappers
-    d_named = layers.Lambda(lambda x: x, name='deep_named')(deep_out)
-    l_named = layers.Lambda(lambda x: x, name='lin_named')(lin_out)
+    # Attach loss layers
+    d_named = IdentityLayer(name='deep_named')(deep_out)
+    l_named = IdentityLayer(name='lin_named')(lin_out)
 
     d_cons = ConsistencyLossLayer(lambda1, name='consistency')([d_named, l_named])
     d_exp = layers.Lambda(lambda x: tf.expand_dims(x, -1), name='expand_deep')(d_cons)
@@ -154,17 +180,23 @@ def build_autoencoder(n_clusters, input_dim, lambda1=0.5, lambda2=0.6):
 
 
 class AutoencoderModel:
+    """
+    Wrapper for easier model management.
+    """
     def __init__(
         self,
         n_clusters,
         input_shape,
         lambda1,
         lambda2,
-        linear_l1=None,
-        linear_l2=None
+        linear_l1=1e-5,
+        linear_l2=1e-3
     ):
         input_dim = input_shape[0]
-        self.model = build_autoencoder(n_clusters, input_dim, lambda1, lambda2)
+        self.model = build_autoencoder(
+            n_clusters, input_dim, lambda1=lambda1, lambda2=lambda2,
+            linear_l1=linear_l1, linear_l2=linear_l2
+        )
 
     def compile(self, *args, **kwargs):
         return self.model.compile(*args, **kwargs)
@@ -186,8 +218,6 @@ class AutoencoderModel:
             'MeanSquaredError': MeanSquaredError
         }
         model = load_model(filepath, custom_objects=custom_objs)
-        inst = cls(0, (model.input_shape[1],1), 0, 0)
+        inst = cls(0, (model.input_shape[1], 1), 0, 0)
         inst.model = model
         return inst
-
-
